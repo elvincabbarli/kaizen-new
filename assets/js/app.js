24bec8a80e0b93aadb15fdb157373da9b82ab809
elvin <elvin@gmail.com>
2023-06-07 15:20:28 +0400

    ss

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,34 @@
-
-const openMenyu = document.getElementById('open-menu')
-openMenyu.addEventListener('click' , () => {
-    let hiddenMenu = document.querySelector('.hidden-menyu')
-    hiddenMenu.classList.add('hidden-active')
-})
-
-const closeMenyu = document.querySelector('.close-hidden')
-closeMenyu.addEventListener('click' , () => {
-    let hiddenMenu = document.querySelector('.hidden-menyu')
-    hiddenMenu.classList.remove('hidden-active')
-})
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    nav: true,  
-    navText: ["<img src='../img/paper/navText.png' />", "<img src='../img/paper/navText.png' />"],  
-    navClass: ['owl-prev', 'owl-next'],
-    responsive:{
-        0:{
-            items:1,
-        },
-        600:{
-            items:1,
-        },
-        1000:{
-            items:1,
-        }
-    }
+const openMenyu = document.getElementById("open-menu");
+openMenyu.addEventListener("click", () => {
+  let hiddenMenu = document.querySelector(".hidden-menyu");
+  hiddenMenu.classList.add("hidden-active");
 });
 
+const closeMenyu = document.querySelector(".close-hidden");
+closeMenyu.addEventListener("click", () => {
+  let hiddenMenu = document.querySelector(".hidden-menyu");
+  hiddenMenu.classList.remove("hidden-active");
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsiveClass: true,
+  nav: true,
+  navText: [
+    "<img src='../img/paper/navText.png' />",
+    "<img src='../img/paper/navText.png' />",
+  ],
+  navClass: ["owl-prev", "owl-next"],
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
