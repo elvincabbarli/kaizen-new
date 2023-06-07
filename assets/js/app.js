d6d9f4d8cf1dd9fc4bbc9ec4b73d6f81db7db374
Haciyev ilkin <ilkinhaciyev955@gmail.com>
2023-06-07 16:06:58 +0400

    carousel fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,25 +17,3 @@ openSearch.addEventListener('click' , () => {
   hiddenSearch.classList.toggle('open-searchbar')
 })
 
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  responsiveClass: true,
-  nav: true,
-  navText: [
-    "<img src='../img/paper/navText.png' />",
-    "<img src='../img/paper/navText.png' />",
-  ],
-  navClass: ["owl-prev", "owl-next"],
-  responsive: {
-    0: {
-      items: 1,
-    },
-    600: {
-      items: 1,
-    },
-    1000: {
-      items: 1,
-    },
-  },
-});
